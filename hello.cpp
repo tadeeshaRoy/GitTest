cf62662ac4b8a8bb69ccc1e1f0e0ff9ba63a2cd2
Roy <tadeeroy@8c85901c4cb3.ant.amazon.com>
2018-01-15 16:04:45 +0530

    Removed variable from hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int l=nums.size(),s=l-1,i=0,a,b;
-        vector<int> result;
         while(i<l)
         {
             if(nums[i]+nums[s]==target)
